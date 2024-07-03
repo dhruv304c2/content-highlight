2e2df90206518d5ee8d26041956a52d02da89951
DhruvPant <dhruv304c2@gmail.com>
2024-07-03 22:33:59 +0530

    video download service now downloads into download folder

--- src/services/vid_download_service.rs
+++ src/services/vid_download_service.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{env, io, process::Command};
 
 
 pub struct VidDownloadService{
@@ -6,31 +6,31 @@ pub struct VidDownloadService{
 }
 
 impl VidDownloadService{
-    pub fn download (url: String) {
-        let output = Command::new("yt-dlp")
+    pub fn download (url: String) -> io::Result<()>{
+        let current_dir = env::current_dir()?;
+        let download_dir_path = format!("{}/downloads", current_dir.to_str().unwrap()).to_string();
+
+        println!("downloads path set to {}", download_dir_path.to_string());
+
+        let _make_dir_output = Command::new("mkdir")
+            .arg("-p")
+            .arg(download_dir_path.clone())
+            .output()
+            .expect("failed to create downloads directory");
+ 
+        let download_output = Command::new("yt-dlp")
             .arg(url.clone())
+            .current_dir(&download_dir_path.clone())
             .output()
             .expect("failed to invoke download command, please ensure you, have yt-dlp installed");
-        if output.status.success() {
-            println!("yt-dlp operation wase successful: returned {}" , String::from_utf8_lossy(&output.stdout));
+
+        if download_output.status.success() {
+            println!("yt-dlp operation was successful: returned {}" , String::from_utf8_lossy(&download_output.stdout));
         }
         else{
-            println!("yt-dlp operation failed returned: {}" , String::from_utf8_lossy(&output.stdout));
+            println!("yt-dlp operation failed returned: {}" , String::from_utf8_lossy(&download_output.stdout));
         }
 
-        let makingdirs = Command::new("mkdir")
-            .arg("-p")
-            .arg("Downloads")
-            .output()
-            .expect("Failed to create downloads directory");
-        
-        let changingdirs = Command::new("mv")
-            .arg("*.webm")
-            .arg("Downloads/")
-            .output()
-            .expect("Files failed to move");
-        if changingdirs.status.success() {
-            print!("Moved to Downloads folder")
-        }
+        Ok(())
     }
 }
