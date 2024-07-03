745cd8f3543958bfed4451624117fc18dca3b105
sidthebit <saditya495@gmail.com>
2024-07-03 21:34:04 +0530

    Making downloads directory aand moving the file to it

--- src/services/vid_download_service.rs
+++ src/services/vid_download_service.rs
@@ -10,13 +10,27 @@ impl VidDownloadService{
         let output = Command::new("yt-dlp")
             .arg(url.clone())
             .output()
-            .expect("failed to invoke dowload command, please ensure you, have yt-dlp installed");
-
+            .expect("failed to invoke download command, please ensure you, have yt-dlp installed");
         if output.status.success() {
             println!("yt-dlp operation wase successful: returned {}" , String::from_utf8_lossy(&output.stdout));
         }
         else{
             println!("yt-dlp operation failed returned: {}" , String::from_utf8_lossy(&output.stdout));
+        }
+
+        let makingdirs = Command::new("mkdir")
+            .arg("-p")
+            .arg("Downloads")
+            .output()
+            .expect("Failed to create downloads directory");
+        
+        let changingdirs = Command::new("mv")
+            .arg("*.webm")
+            .arg("Downloads/")
+            .output()
+            .expect("Files failed to move");
+        if changingdirs.status.success() {
+            print!("Moved to Downloads folder")
         }
     }
 }
